99bbd09fd66092266e70c684e867b64f31a42946
mosesvk <kaumatule93@gmail.com>
2022-07-16 08:17:44 -0600

    _mernBlog frontend URL params (parameters) through routes

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,23 +1,35 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-import { useParams } from 'react-router-dom'
-import articleContent from './article-content'
+import ArticlesList from '../components/ArticlesList';
+import articleContent from './article-content';
 
-const ArticlePage = () => {
-  const params = useParams()
-  const name = params.name
-  const article = articleContent.find((article) => article.name === name)
+const ArticlePage = ({ match }) => {
+  const params = useParams();
+  const name = params.name;
 
-  if (!article) {
-    return <h1>No Article Found!</h1>
-  }
+  const article = articleContent.find((article) => article.name === name);
+
+  if (!article) return <h1>Article does not exist!</h1>;
+
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
+
   return (
     <>
-      <h1>{article.title}</h1>
-      {article.content.map((paragraph, idx) => (
-        <p key={idx}>{paragraph}</p>
-      ))}
+      <div>
+        <h1 className='text-3xl font-bold mb-4'>{article.title}</h1>
+        {article.content.map((paragraph, key) => (
+          <p className='mb-4' key={key}>
+            {paragraph}
+          </p>
+        ))}
+      </div>
+      <h3>Other Articles:</h3>
+      <ArticlesList articles={otherArticles} />
     </>
-  )
-}
+  );
+};
 
-export default ArticlePage
+export default ArticlePage;
